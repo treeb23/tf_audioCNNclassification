8ad53230c4d63503221bc8c861c8e3a3268cde48
treeb23 <105105490+treeb23@users.noreply.github.com>
2023-04-09 23:16:27 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='W2VBiLSTM',
+    name='tf_audioCNNclassification',
     version='0.0.1',
     long_description=long_description,
     long_description_content_type='text/markdown',
